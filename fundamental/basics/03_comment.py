c9b532d8159784d20093ceed339d2805f0bef46c
krish <krishbishnoi79@gmail.com>
2026-06-06 22:04:18 +0530

    Add files via upload

---
+++ fundamental/basics/03_comment.py
@@ -0,0 +1,7 @@
+print("hi this is krish bishnoi")
+# print("hi thtis is python here ")
+print("hi i am python")
+"""
+hi this is a multiline comment . everything wirtten btw this would be ignored by python
+
+"""
