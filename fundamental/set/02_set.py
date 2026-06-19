a0cd88f792b27217f195625f31a84c6edd439b7c
krish <krishbishnoi79@gmail.com>
2026-06-19 12:40:19 +0530

    Add files via upload

---
+++ fundamental/set/02_set.py
@@ -0,0 +1,15 @@
+fruits={"apple","kela","krish","heloo","somthing"}
+print(fruits)
+fruits.remove("apple")
+print(fruits)
+fruits.add("apple")
+print(fruits)
+fruits.update("krish23","me123","heisgoodbpy","hewantobeasde")
+a,b=input("check if weather these itmens in set or not ").split()
+print(a in fruits)
+print(b in fruits)
+vegetable={"apple","kela","krish want to be a good programer","me123","hi krish how are you"}
+print(fruits.union(vegetable))
+print(fruits.intersection(vegetable))
+print(fruits-vegetable)
+''' aur haan . set ke ander vo sum, max,min and sort vo wale bhi hote hai '''
---
+++ fundamental/set/03_practice.py
@@ -0,0 +1,10 @@
+sabdkosh= {
+    "fan": "pankha",
+    "table": "mej",
+    "chair" :"kurshi",
+    "mouse":"chuha",
+    "pen":"kalam",
+    "curtians":"prde",
+}
+word=input("please entre the word you wnat to seracvh in the this")
+print(sabdkosh[word])
