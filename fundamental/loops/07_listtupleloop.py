bafbe2154db44c5d486fb223288cf3e8f7368767
krish <krishbishnoi79@gmail.com>
2026-06-20 18:24:17 +0530

    Add files via upload

---
+++ fundamental/loops/07_listtupleloop.py
@@ -0,0 +1,6 @@
+a=(2,34,4,56,34,32,34,54,67,54,34,32,32,34,54,6)
+for i in range(0,len(a)) :
+    print(a[i])
+# also we can do thsi 
+for i in a :
+    print(i)
---
+++ fundamental/loops/09_continue_breake_pass.py
@@ -0,0 +1,6 @@
+for i in range(30):
+    print(i)
+    if(i==2):
+        continue
+    if (i==20):
+     break 
